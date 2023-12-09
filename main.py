476cba63205d08ed2a93fb4dfb1aebe6169641f5
ForestLee0513 <woolimlee.dev@gmail.com>
2023-12-09 17:07:57 +0900

    [feat] 여러 상품 정보 요청기능 추가

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from src import get_product_info
 app = Flask(__name__)
 
 url = "https://smartstore.naver.com/geonlab/products/9031635718"
 
+@app.route("/get-products")
+def get_products():
+    data = request.json
+    product_info_list = []
+
+    # data.url의 형식이 리스트가 아닐 경우 에러 반환
+    if type(data['url']) != list:
+        return "Invalid request, please check document."
+    
+    for product_url in data['url']:
+        product_info_list.append(get_product_info.get_product_info(product_url))
+    
+    return jsonify({
+        "productInfo": product_info_list
+    })
+
 @app.route('/')
 def main():
-    print(get_product_info.get_product_info(url))
-
-    return "Everything is good"
+    return "NAVER Smartstore crawler is running.."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001', debug=True)
